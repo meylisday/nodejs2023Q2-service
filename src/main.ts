2221262e64cfa46d8a444d0ec0be4c8df6eb5123
meylisday <victory.sett@gmail.com>
2023-07-31 09:52:59 +0200

    Update docs

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import * as fs from 'node:fs';
+import * as yaml from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const config = new DocumentBuilder()
-    .setTitle('Home Library Service')
-    .setDescription('Home music library service')
-    .setVersion('1.0')
-    .addTag('Home Library Service')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doc', app, document);
+  const swaggerDocument = yaml.load(fs.readFileSync('./doc/api.yaml', 'utf8'));
+  SwaggerModule.setup('doc', app, swaggerDocument as OpenAPIObject);
   await app.listen(4000);
 }
 bootstrap();
